594b58f11bdc1bef8f5ff3d5db8a52f45dd63d9f
Saturday-365 <297872990@qq.com>
2025-05-12 18:07:13 +0800

    5月12日 累了 要考试了 感觉还是算了吧

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,8 +7,8 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     setWindowTitle("PhotoShop Lite"); //设置窗口标题
-    //ui->mode1Btn->setFixedSize(50,50);//设置按钮大小
-    ui->mode1Btn->setIcon(QIcon(":/Icon/STAR.PNG"));//设置按钮图标
+    ui->mode1Btn->setFixedSize(200,80);//设置按钮大小
+    ui->mode1Btn->setIcon(QIcon(":/Icon/waiting.png"));//设置按钮图标
     ui->mode1Btn->setIconSize(QSize(ui->mode1Btn->width(),ui->mode1Btn->height()));//设置按钮图标大小
     ui->mode1Btn->setStyleSheet("background-color:transparent");
 
